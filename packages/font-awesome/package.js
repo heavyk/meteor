837711df70d88f07d5a286486eaf0bec168a90f8
flames of love <mechanicofthesequence@gmail.com>
2012-10-08 07:54:46 +0200

    a bunch of missing font awesome stuff

---
+++ packages/font-awesome/package.js
@@ -0,0 +1,12 @@
+Package.describe({
+  summary: "The iconic font designed for use with Twitter Bootstrap"
+});
+
+Package.on_use(function (api) {
+  console.log(api, Package)
+  api.add_files('css/font-awesome.css', 'client');
+  api.add_files('font/fontawesome-webfont.eot', 'client');
+  api.add_files('font/fontawesome-webfont.woff', 'client');
+  api.add_files('font/fontawesome-webfont.ttf', 'client');
+  api.add_files('font/fontawesome-webfont.svg', 'client');
+});
