35835f4b59a8cbbd6bec5063158f9f1b5ddc579b
flames of love <mechanicofthesequence@gmail.com>
2012-10-22 05:30:11 +0200

    font-awesome stuff

--- packages/font-awesome/package.js
+++ packages/font-awesome/package.js
@@ -3,7 +3,6 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
-  console.log(api, Package)
   api.add_files('css/font-awesome.css', 'client');
   api.add_files('font/fontawesome-webfont.eot', 'client');
   api.add_files('font/fontawesome-webfont.woff', 'client');
